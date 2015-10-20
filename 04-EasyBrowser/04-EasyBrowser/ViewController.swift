2045a121c5336e947aa19fe7cd2492264c1a2371
Laura C <lauramocanu16@gmail.com>
2015-10-20 12:34:02 +0300

    add 04-Easy Browser

---
+++ 04-EasyBrowser/04-EasyBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  04-EasyBrowser
+//
+//  Created by Laura Calinoiu on 20/10/15.
+//  Copyright © 2015 3Smurfs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
