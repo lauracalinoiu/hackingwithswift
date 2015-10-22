da2232f10cd4f9eafc0dd6bfb52367298fb79f8e
Laura C <lauramocanu16@gmail.com>
2015-10-22 12:22:26 +0300

    add autolayout to flags project

---
+++ 06-AutoLayoutForFlags/02-GuessTheFlag/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  02-GuessTheFlag
+//
+//  Created by Laura Calinoiu on 20/10/15.
+//  Copyright © 2015 3Smurfs. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button2.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button3.layer.borderColor = UIColor.lightGrayColor().CGColor
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        askQuestion()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func askQuestion(alertAction: UIAlertAction! = nil){
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+        
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        title = countries[correctAnswer].uppercaseString
+    }
+    
+    
+    @IBAction func buttonTapped(sender: UIButton) {
+        var messageTitle: String
+        
+        if sender.tag == correctAnswer{
+            messageTitle = "Correct"
+            ++score
+        } else {
+            messageTitle = "Wrong"
+            --score
+        }
+        
+        let message = "Your score is \(score)"
+        let ac = UIAlertController(title: messageTitle, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        ac.addAction(UIAlertAction(title: "Dismiss", style: .Cancel, handler: askQuestion))
+        presentViewController(ac, animated: true, completion: nil)
+    }
+}
+
