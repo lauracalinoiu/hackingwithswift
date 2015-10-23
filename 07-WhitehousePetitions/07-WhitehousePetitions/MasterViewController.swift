105b2073a6553020f20e77d497433a2be81a3e62
Laura C <lauramocanu16@gmail.com>
2015-10-23 12:07:07 +0300

    connect to whitehouse petitions, and parse

--- 07-WhitehousePetitions/07-WhitehousePetitions/MasterViewController.swift
+++ 07-WhitehousePetitions/07-WhitehousePetitions/MasterViewController.swift
@@ -9,28 +9,52 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+    
     var detailViewController: DetailViewController? = nil
-    var objects = [AnyObject]()
-
-
+    var objects = [[String: String]]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        
+        if let url = NSURL(string: urlString){
+            if let data = try? NSData(contentsOfURL: url, options: []){
+                let json = JSON(data: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200{
+                    parseJSON(json)
+                }
+            }
+        }
     }
-
+    
+    func parseJSON(json: JSON){
+        for result in json["results"].arrayValue{
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            
+            let dict = ["title": title, "body": body, "sigs": sigs]
+            objects.append(dict)
+        }
+        
+        
+        tableView.reloadData()
+    }
+    
     override func viewWillAppear(animated: Bool) {
         self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
         super.viewWillAppear(animated)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Segues
-
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "showDetail" {
             if let indexPath = self.tableView.indexPathForSelectedRow {
@@ -42,26 +66,24 @@ class MasterViewController: UITableViewController {
             }
         }
     }
-
-    // MARK: - Table View
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return objects.count
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
+        
         let object = objects[indexPath.row]
         cell.textLabel!.text = object.description
         return cell
     }
-
-
-
+    
+    
+    
 }
 
