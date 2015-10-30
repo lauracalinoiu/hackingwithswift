2dfb28b644a840d7bd54dc3ed6e7aa1bec95c363
Laura C <lauramocanu16@gmail.com>
2015-10-30 10:48:20 +0200

    initial empty SpriteKit project

---
+++ 11-Pachinko/11-Pachinko/GameScene.swift
@@ -0,0 +1,26 @@
+//
+//  GameScene.swift
+//  11-Pachinko
+//
+//  Created by Laura Calinoiu on 30/10/15.
+//  Copyright (c) 2015 3Smurfs. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .Replace
+        background.zPosition = -1
+        addChild(background)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
